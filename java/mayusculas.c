2a4804d57fa985aacd6b04de43c066f3ce249873
santi <srlopezh@gmail.com>
2019-10-05 13:03:17 +0200

    Java y C

---
+++ java/mayusculas.c
@@ -0,0 +1,27 @@
+/* 
+
+Realizado para ser invocado desde Mayusculas.java
+Lo dejamos como a.out
+
+Si lo ejecutamos convierte cada linea de minúsculas a mayusculas
+
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+int main(int argc, char **argv)
+{
+    char buf[1024];
+    int longitud;
+    int i;
+    while(fgets(buf, 1024, stdin) != NULL) {
+        longitud = strlen(buf);
+        for(i = 0; i < longitud; i++) {
+            buf[i] = toupper(buf[i]);
+        }
+        fputs(buf, stdout);
+        fflush(stdout); // IMP: para asegurar que se reciben los datos
+    }
+    return 0;
+}
