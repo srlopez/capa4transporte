b4c5f53fe0bf624b00daeee58ed2ce9ed4b54ac8
santi <srlopezh@gmail.com>
2019-10-14 09:38:46 +0200

    C thread race conditions

--- c/c302Threads.c
+++ c/c302Threads.c
@@ -4,56 +4,54 @@
 #include <string.h>
 #include <unistd.h>
 
-
 struct thread_vars_t
 {
   int number;
 };
 
-
 void longTask()
 {
-  /* A time taking task */
-  sleep(rand()%2);
+  /*  */
+  sleep(rand() % 2);
 }
-
 
 void *newtask(void *_number)
 {
   struct thread_vars_t *vars = _number;
 
   int number = vars->number;
-  longTask();
-  vars->number = number+1;
+  longTask(); // usaríamos number dentro de la tarea
+  vars->number = number + 1;
 
-  printf ("THREAD id = %d\n", vars->number);
+  printf("THREAD number = %d\n", vars->number);
 
   pthread_exit(NULL);
 }
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-   pthread_t thread;
-   int rc;
-   int i;
-   struct thread_vars_t *vars = malloc (sizeof(struct thread_vars_t));
+  pthread_t thread;
+  int rc;
+  int i;
+  struct thread_vars_t *vars = malloc(sizeof(struct thread_vars_t));
 
-   vars->number = 0;
+  vars->number = 0;
 
-   printf ("Main recien empezado.\n");
-   for (i=0; i<10; ++i)
-     {
-       rc = pthread_create(&thread, NULL, newtask, vars);
-       if (rc)
-     {
-       printf("ERROR en pthread_create(): %d\n", rc);
-       exit(-1);
-     }
-     }
+  printf("Main recien empezado.\n");
+  for (i = 0; i < 10; ++i)
+  // al lanzar 10 sumas deberíamos obtener el resultado de 10
+  {
+    rc = pthread_create(&thread, NULL, newtask, vars);
+    if (rc)
+    {
+      printf("ERROR en pthread_create(): %d\n", rc);
+      exit(-1);
+    }
+  }
 
   longTask();
 
-   printf ("Main a punto de acabar.\n");
-   /* Last thing that main() should do */
-   pthread_exit(NULL);
+  printf("Main a punto de acabar.\n");
+  /* Lo último en main() que deberíamos hacer */
+  pthread_exit(NULL);
 }
