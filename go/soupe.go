7c6647d5fca2401d78fe1402dac03f8205d1de9b
santi <srlopezh@gmail.com>
2019-11-03 18:03:45 +0100

    Test con Go

---
+++ go/soupe.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+type T = struct {
+	id int
+	c  rune
+}
+
+var cubiertos = make(chan rune, 2)
+
+var messages [5]chan T
+var status [5]chan string
+
+var cantidad int32 = 10
+
+var l1 string = "2334400112"
+var l2 = []byte{'.', '.', '.', '.', '.', '.', '.', '.', '.', '.', 0}
+var l3 string = "0011223344"
+
+var mutex = &sync.Mutex{}
+
+func f1(id int) {
+	for {
+
+		// Si tengo mensajes me los como
+		select {
+		case msg := <-messages[id]:
+			c := atomic.AddInt32(&cantidad, -1)
+			i := id + msg.id + (func() int {
+				if msg.id < id {
+					return -2
+				} else {
+					return 3
+				}
+			})()
+			mutex.Lock()
+			fmt.Println("----", c, "----")
+			l2[i] = string(msg.c)[0]
+			fmt.Println(l1)
+			fmt.Println(fmt.Sprintf("%s", l2))
+			fmt.Println(l3)
+			mutex.Unlock()
+			time.Sleep(time.Second) //Comiendo
+			mutex.Lock()
+			l2[i] = '.'
+
+			mutex.Unlock()
+
+			status[id] <- "ready"
+		default:
+		}
+
+		// Si tengo cuchara
+		select {
+		case cuchara := <-cubiertos:
+			// Envio un mensaje
+			h := -1
+			select {
+			case <-status[(id+2)%5]:
+				h = (id + 2) % 5
+				messages[h] <- T{id, cuchara} //fmt.Sprintf("%d%s%d", id, cuchara, h)
+				status[h] <- <-status[h]
+			case <-status[(id+3)%5]:
+				h = (id + 3) % 5
+				messages[h] <- T{id, cuchara} //fmt.Sprintf("%d%s%d", id, cuchara, h)
+				status[h] <- <-status[h]
+
+			default:
+			}
+
+			cubiertos <- cuchara
+
+		default:
+
+		}
+
+	}
+	fmt.Println("worker", id, "exit")
+
+}
+
+func main() {
+
+	cubiertos <- 'a'
+	cubiertos <- 'b'
+
+	for i := 0; i <= 4; i++ {
+		messages[i] = make(chan T, 1)
+		status[i] = make(chan string, 1)
+		status[i] <- "ready" //antes de lanzar la rutina
+
+	}
+
+	for w := 0; w <= 4; w++ {
+		go f1(w)
+	}
+	for atomic.LoadInt32(&cantidad) > 0 {
+	}
+	fmt.Println("end")
+
+}
